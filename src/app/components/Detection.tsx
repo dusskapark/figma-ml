c3cdf63180a347b3b9faa212f5975733cf4a80c3
JooHyung Park <dusskapark@gmail.com>
2021-12-03 11:11:03 +0900

    Added logic to prevent TFJS loop

--- src/app/components/Detection.tsx
+++ src/app/components/Detection.tsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import {uint8ArrayToObjectURL, runPredict} from './ImageHandling';
 
 const Detection = (props: {item; model; classesDir}) => {
+    const [imgId, setImgId] = React.useState(null);
     const {item, model, classesDir} = props;
-    // const [imgLoaded, setImgLoad] = React.useState(false);
-
     const imgRef = React.useRef<HTMLImageElement>(null);
     const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
-    const handleImgLoad = () => {
+    const handleImgLoad = (id: string) => {
         console.log('image loaded...');
-        runPredict(imgRef.current, canvasRef.current, model, classesDir);
+        if (imgId !== id) {
+            runPredict(imgRef.current, canvasRef.current, model, classesDir);
+            setImgId(id);
+        }
     };
 
     return (
@@ -18,7 +20,7 @@ const Detection = (props: {item; model; classesDir}) => {
             <img
                 ref={imgRef}
                 style={{position: 'absolute'}}
-                onLoad={handleImgLoad}
+                onLoad={() => handleImgLoad(item.id)}
                 src={uint8ArrayToObjectURL(item.data)}
                 alt={item.path}
                 id={item.id}
